d75a8a42bd6fb9ca68fd79b98a32178c38418572
QuocTuanJV <quoctuan.naf@gmail.com>
2019-10-24 09:04:24 +0700

    Point 2D and Point 3D

---
+++ src/com/tuan/point2DAndpoint3D/MainPoint.java
@@ -0,0 +1,10 @@
+package com.tuan.point2DAndpoint3D;
+
+public class MainPoint {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(1,2);
+        Point3D point3D = new Point3D(3,4,5);
+        System.out.println(point2D);
+        System.out.println(point3D);
+    }
+}
