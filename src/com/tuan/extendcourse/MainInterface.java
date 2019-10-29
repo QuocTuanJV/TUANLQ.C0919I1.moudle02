0740bc40a330534c438f5b71568000fff84ed328
QuocTuanJV <quoctuan.naf@gmail.com>
2019-10-30 02:29:11 +0700

    Delete demo test students

--- src/com/tuan/extendcourse/MainInterface.java
+++ src/com/tuan/extendcourse/MainInterface.java
@@ -3,16 +3,17 @@ package com.tuan.extendcourse;
 public class MainInterface {
     public static void main(String[] args) {
         Shape[] shapes = new Shape[3];
-        shapes[0] =  new Circle(2,"green", true);
-        shapes[1] = new Rectangle(12,10,"green",true);
+         shapes[0] =  new Circle(2,"green", true);
+         shapes[0] = new Rectangle(12,10,"green",true);
+        ((Circle)shapes[0]).resize123(12.0);
 
-        for(Shape shape: shapes){
-            if(shape instanceof Circle){
-                ((Circle) shape).resize123(100);
-            } else if(shape instanceof Rectangle){
-                ((Rectangle) shape).resize123(200);
-            }
-        }
+//        for(Shape shape: shapes){
+//            if(shape instanceof Circle){
+//                ((Circle) shape).resize123(100);
+//            } else if(shape instanceof Rectangle){
+//                ((Rectangle) shape).resize123(200);
+//            }
+//        }
 
 
     }
